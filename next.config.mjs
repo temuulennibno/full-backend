6a52dfd97eb8734cf2376070c0a89051224e1ee2
Temuulen Narmandakh <temuulen.n@pinecone.mn>
2024-03-18 17:22:48 +0800

    fix cors url

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/api/.*",
+        source: "/api/graphql",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
